2ae7f9ff8f84ecb33c07c469069cc1b2923db904
GoshLee <2366861982@qq.com>
2017-12-20 08:56:22 +0800

    新闻文章controller注解

--- src/cn/staryu/controller/NewsArticlesController.java
+++ src/cn/staryu/controller/NewsArticlesController.java
@@ -1,5 +1,16 @@
 package cn.staryu.controller;
 
+import cn.staryu.service.news_articles.NewsArticlesService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.annotation.Resource;
+
+@Controller
+@RequestMapping("/newsAtricles")
 public class NewsArticlesController {
+
+    @Resource
+    NewsArticlesService newsArticlesService;
 
 }
