0234dce03a33c03117cd72e4560007a8fcf51073
kong-kxz <1172068873@qq.com>
2017-12-20 14:40:58 +0800

    用户登录、登出

--- src/cn/staryu/controller/AdminUserController.java
+++ src/cn/staryu/controller/AdminUserController.java
@@ -1,9 +1,51 @@
 package cn.staryu.controller;
 
+import cn.staryu.pojo.AdminUser;
+import cn.staryu.service.admin_user.AdminUserService;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 /**
  * @author kongxiangzhong
  * 2017/12/19 15:52
  */
 public class AdminUserController {
+
+    @Resource
+    AdminUserService adminUserService;
+
+    @RequestMapping(value="/login")//登录
+    public String login(@ModelAttribute("adminUser") AdminUser adminUser , HttpSession session, HttpServletRequest request){
+        AdminUser adminUser1=null;
+        try {
+            adminUser1=adminUserService.finduserLogin(adminUser.getAdminName(),adminUser.getAdminPassword());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        if(adminUser1 != null){
+            //放入session
+            session.setAttribute("adminUserSession", adminUser1);
+
+
+            return "developer/frame";
+        }else{
+            request.setAttribute("error","用户名或密码不正确！");
+
+            return "backend/login";
+        }
+
+
+    }
+    @RequestMapping("/loginout")
+    public String loginout(HttpServletRequest request){//登出
+        //    注销用户，使session失效。
+        request.getSession().removeAttribute("adminUserSession");
+
+        return "backend/login";
+    }
 
 }
--- src/cn/staryu/controller/DevUserController.java
+++ src/cn/staryu/controller/DevUserController.java
@@ -7,7 +7,8 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.annotation.Resource;
-
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 
 /**
@@ -17,6 +18,42 @@ import javax.annotation.Resource;
 @Controller
 @RequestMapping("/devuser")
 public class DevUserController {
+
+    @Resource
+    DevUserService devUserService;
+
+    @RequestMapping(value="/login")//登录
+    public String login(@ModelAttribute("devUser") DevUser devUser , HttpSession session, HttpServletRequest request){
+        DevUser devUser1=null;
+        try {
+            devUser1=devUserService.finduserLogin(devUser.getDevName(),devUser.getDevPassword());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        if(devUser1 != null){
+            //放入session
+            session.setAttribute("devUserSession", devUser1);
+
+
+            return "developer/frame";
+        }else{
+            request.setAttribute("error","用户名或密码不正确！");
+
+            return "developer/login";
+        }
+
+
+    }
+    @RequestMapping("/loginout")
+    public String loginout(HttpServletRequest request){//登出
+        //    注销用户，使session失效。
+        request.getSession().removeAttribute("devUserSession");
+
+        return "developer/login";
+    }
+
+
+
 
 
 
